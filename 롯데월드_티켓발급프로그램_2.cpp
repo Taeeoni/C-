ac615dae404d9d5efd889005f1598bb72dc4a388
Taeeoni <102118666+Taeeoni@users.noreply.github.com>
2022-04-15 14:06:30 +0900

    Add files via upload

--- 롯데월드_티켓발급프로그램_2.cpp
+++ 롯데월드_티켓발급프로그램_2.cpp
@@ -1,5 +1,7 @@
 #include <stdio.h>
-
+	
+	int yearMonthDay = 20220414;
+	
 	// 종합이용권 가격
 	const int ALL_1D_ADULT = 62000, ALL_1D_TEEN = 54000 ,ALL_1D_KID = 47000 ,ALL_1D_BABY = 15000;	
 	const int ALL_A4_ADULT = 50000, ALL_A4_TEEN = 43000, ALL_A4_KID = 36000, ALL_A4_BABY = 15000;
@@ -16,7 +18,7 @@
 	const int MIN_BABY = 1, MIN_KID = 3, MIN_TEEN = 12, MIN_ADULT = 18, MIN_OLD = 65; 
 	
 	// 할인율
-	const int DISABLED = 0.5, NATIONAL_MERIT = 0.5, SOLDIER = 0.49, PREGNANT = 0.5, MANY_CHILDREN = 0.3;
+	const double DISABLED = 0.5, NATIONAL_MERIT = 0.5, SOLDIER = 0.49, PREGNANT = 0.5, MANY_CHILDREN = 0.3;
 
 	
 void errorMessagePrint(){
@@ -51,9 +53,30 @@ int orderCitizenNum(int citizenNum){
 	citizenNum += PROTECT_CITIZEN_NUM;
 }
 
+int orderTicketCount(int ticketCount){
+	printf("몇개를 주문하시겠습니까?(최대 10개)\n");
+	scanf("%d", &ticketCount);
+	return ticketCount;
+}
+
+int orderPreferClass(int preferClass){
+	
+	while(preferClass < 1 || preferClass > 6){
+	printf("우대사항을 선택하세요.\n");
+	printf("1. 없음 (나이 우대는 자동처리)\n");
+	printf("2. 장애인 우대\n"); //동반1인 
+	printf("3. 국가유공자 우대\n"); //동반1인 
+	printf("4. 휴가장병 우대\n"); //동반1인 
+	printf("5. 임산부 우대\n");
+	printf("6. 다둥이 행복카드 우대\n");
+	scanf("%d", &preferClass);
+	}
+	return preferClass;	
+}
+
 
 int calcAge(int citizenNum){
-	int today = 20220414; // 오늘 날짜 
+	int today = yearMonthDay; // 오늘 날짜 
 	int gender = citizenNum % 10; // 주민번호 뒷자리 1, 2, 3, 4 
 	int birthDay = citizenNum / 10;
 	if (gender >= 3){
@@ -84,7 +107,6 @@ int calcAgeGroup(int manAge){
 	}
 	return ageGroup;
 }	
-
 
 int calcPriceProcess(int ageGroup, int ticketClass, int ticketType){
 	if(ticketClass == 1){ // ALL
@@ -155,28 +177,10 @@ int calcPriceProcess(int ageGroup, int ticketClass, int ticketType){
 	}
 }
 
-int orderTicketCount(int ticketCount, int price){
-	printf("몇개를 주문하시겠습니까?(최대 10개)\n");
-	scanf("%d", &ticketCount);
+int calcTicketCount(int ticketCount, int price){
 	price *= ticketCount;
 	return price;
-}
-
-int orderPreferClass(int preferClass){
-	
-	while(preferClass < 1 || preferClass > 6){
-	printf("우대사항을 선택하세요.\n");
-	printf("1. 없음 (나이 우대는 자동처리)\n");
-	printf("2. 장애인 우대\n");
-	printf("3. 국가유공자 우대\n");
-	printf("4. 휴가장병 우대\n");
-	printf("5. 임산부 우대\n");
-	printf("6. 다둥이 행복카드 우대\n");
-	scanf("%d", &preferClass);
-	}
-	
-	return preferClass;	
-}
+} 
 
 int calcDiscount(int price, int preferClass, int ticketClass){
 	if(preferClass == 2){
@@ -193,7 +197,9 @@ int calcDiscount(int price, int preferClass, int ticketClass){
 	return price;
 }
 
-void savedInforms (int ticketClass, int ticketType, int ageGroup, int ticketCount, int price, int preferClass, int position, 
+
+
+int *savedInforms (int ticketClass, int ticketType, int ageGroup, int ticketCount, int price, int preferClass, int position, 
 					int inputList [][6]){
 	
 	inputList[position][0] = ticketClass;
@@ -202,6 +208,7 @@ void savedInforms (int ticketClass, int ticketType, int ageGroup, int ticketCoun
 	inputList[position][3] = ticketCount;
 	inputList[position][4] = price;
 	inputList[position][5] = preferClass;
+
 }
 
 
@@ -221,7 +228,8 @@ void inputData(int position, int inputList[][6]){
 	manAge = calcAge(citizenNum);
 	ageGroup = calcAgeGroup(manAge);
 	price = calcPriceProcess(ageGroup, ticketClass, ticketType);
-	price = orderTicketCount(0, price);
+	ticketCount = orderTicketCount(0);
+	price = calcTicketCount(ticketCount, price);
 	preferClass = orderPreferClass(0);
 	if(preferClass != 1){
 		price = calcDiscount(price, preferClass, ticketClass); 
@@ -234,26 +242,108 @@ void inputData(int position, int inputList[][6]){
 
 
 
+char* convertTicketClass(int input){
+	char* ticketClass;
+	if(input == 1){
+		ticketClass = "종합이용권"; 
+	} else if(input == 2){
+		ticketClass = "파크이용권";
+	} 
+	return ticketClass;
+}
 
+char* convertTicketType(int input){
+	char* ticketType;
+	if(input == 1){
+		ticketType = "1Day";
+	} else if(input == 2){
+		ticketType = "After4";
+	}
+	return ticketType;
+}
 
-  
-void entrancePrint(int ticketClass, int ticketType, char* ageGroup, int ticketCount, int price, int preferClass){
+char* convertAgeGroup(int input){
+	char* ageGroup;
+	if (input == 0){
+		ageGroup = "영유아";
+	} else if (input == 1){
+		ageGroup = "유아";
+	} else if (input == 2){
+		ageGroup = "어린이";
+	} else if (input == 3){
+		ageGroup = "청소년";
+	} else if (input == 4){
+		ageGroup = "어른";
+	} else if (input == 5){
+		ageGroup = "노인";
+	}
+	return ageGroup;
+}
 
-	printf("티켓 발권을 종료합니다. 감사합니다.\n");
-	printf("%d  %d  %s  %d  %d  %d", ticketClass, ticketType, ageGroup, ticketCount, price, preferClass);
-			
+char* convertPreferClass(int input){
+	char* preferClass;
+	if(input == 1){
+		preferClass = "없음";
+	} else if(input == 2){
+		preferClass = "장애인";
+	} else if (input == 3){
+		preferClass = "국가유공자";
+	} else if (input == 4){
+		preferClass = "휴가장병";
+	} else if (input == 5){
+		preferClass = "임산부";
+	} else if (input == 6){
+		preferClass = "다둥이";
+	}
+	return preferClass;
 }
 
 
+void csvPrint(){
+	FILE *fp;
+	fp = fopen("C:\\test\\lotteworldreport\\report.csv", "w");
+	fprintf(fp, "날짜, 이용권, 권종, 연령구분, 수량, 가격, 우대사항\n");
+}
+
+
+void entrancePrint(int inputList[][6], int position){
+	
+	FILE *fp;
+	fp = fopen("C:\\test\\lotteworldreport\\report.csv", "a");
+	int totalPrice = 0;
+	printf("티켓 발권을 종료합니다. 감사합니다.\n");
+	printf("---------------------롯데월드-----------------------------\n");
+	for(int column = 0; column <= position; column++){
+		
+		char* ticketClass = convertTicketClass(inputList[column][0]);
+		char* ticketType = convertTicketType(inputList[column][1]);
+		char* ageGroup = convertAgeGroup(inputList[column][2]);
+		char* preferClass = convertPreferClass(inputList[column][5]);
+		//ticketClass ticketType ageGroup ticketCount price preferClass 
+		printf("%s %5s %5s %2d장 %8d원 *%s\n", ticketClass, ticketType, ageGroup, inputList[column][3], 
+						inputList[column][4], preferClass);
+		totalPrice += inputList[column][4];
+		
+		fprintf(fp, "%d,%s,%s,%s,%d,%d,%s\n", yearMonthDay, ticketClass, ticketType, ageGroup, inputList[column][3], 
+						inputList[column][4], preferClass);	
+		    
+	}
+	printf("입장료 총액은 %d원 입니다.\n", totalPrice);
+	
+	fclose(fp); 
+}
+
+
+
 int main() {
+	
+	csvPrint();
 	
 	int totalPrice = 0; // 주문 총액 
 	int position = 0; // inputList 행 추가시 사용  
 	int filePosition = 0; // 파일 출력 첫줄 구분 
 	int inputList [100][6] = {0}; // 주문내역 저장 
-	
-	
-	
+	int exitIndex = 0;
 
 	while(true){
 		inputData(position, inputList);	
@@ -263,13 +353,15 @@ int main() {
 		printf("1. 티켓 발권\n");
 		printf("2. 종료\n");
 		scanf("%d", &continueSelect);
-		
-		if(continueSelect == 2){
+		if(continueSelect == 1){
+			exitIndex++;
+		} else if(continueSelect == 2){
+			entrancePrint(inputList, position);	
 			break;
 		}	
 		
 		position++; // 배열위치 변경 
-	} 
+	}
 
 }
 
